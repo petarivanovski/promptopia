5098be4577d6413712d3bc6acc52d7eb76e9e72e
petarivanovski <petar.ivanovski97@gmail.com>
2023-05-10 23:58:46 +0200

    Fixed bug when trying to save user image

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -30,7 +30,7 @@ const handler = NextAuth({
           await User.create({
             email: profile.email,
             username: profile.name.replace(" ", "").toLowerCase(),
-            image: profile.image,
+            image: profile.picture,
           });
         }
 
